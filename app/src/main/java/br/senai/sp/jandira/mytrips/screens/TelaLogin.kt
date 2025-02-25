f692b4c3c62ffbb44f8d27ccef99b50c61814cb0
GABRIEL SOUZA COSTA <24122537@dsn127m1242375.cfp127aluno.local>
2025-02-25 12:11:24 -0300

    Código Corrigido

--- app/src/main/java/br/senai/sp/jandira/mytrips/screens/TelaLogin.kt
+++ app/src/main/java/br/senai/sp/jandira/mytrips/screens/TelaLogin.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -71,7 +72,8 @@ fun TelaLogin(modifier: Modifier = Modifier) {
                 Text(
                     text = stringResource(R.string.app_name),
                     fontSize = 42.sp,
-                    color = Color(0xFF673AB7)
+                    color = Color(0xFF673AB7),
+                    fontWeight = FontWeight.Bold
                 )
                 Text(
                     text = stringResource(R.string.logo_descripition),
